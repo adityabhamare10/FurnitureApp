92961ca48ca179b48f75c262cca32d94f9adbdc0
adityabhamare10 <adityabhamare71@gmail.com>
2023-08-14 13:26:17 +0530

    First commit

---
+++ app/src/main/java/net/androidsquad/furnitureapp/MyData2.java
@@ -0,0 +1,37 @@
+package net.androidsquad.furnitureapp;
+
+public class MyData2 {
+
+
+    static String[] nameArray = {
+            "Chair",
+            "Table",
+            "Room"
+    };
+
+    static String[] versionArray = {
+            "500$",
+            "120$",
+            "1900$"
+    };
+
+
+    static int[] id_ = {1,2,3};
+
+
+    static int[] drawableArray = {
+            R.drawable.fur5,
+            R.drawable.fur2,
+            R.drawable.fur6
+    };
+
+
+
+
+
+
+
+
+
+
+}
